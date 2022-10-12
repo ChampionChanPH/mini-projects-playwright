5ccd49a83e71c85710d311f3bab5d55f07da1b0b
Chan <christian.anasco@prosple.com>
2022-10-12 14:23:36 +0800

    Added CMS tests to update courses

---
+++ tests/cms-update/cms.spec.js
@@ -0,0 +1,63 @@
+const { test, expect } = require('@playwright/test')
+
+test.use({
+    storageState: 'resources/authStateDevCMS.json'
+})
+
+const data = [
+    "https://dev.cms.connect.prosple.com/node/12345"
+]
+
+test.describe('tests done in CMS', async () => {
+    // user successfully login to the CMS via email and password
+    test('successful user login via email and password', async ({ page }) => {
+        await page.goto(`https://dev.cms.connect.prosple.com/`)
+        await page.fill("input[name=email]", `christian.anasco@prosple.com`)
+        await page.fill("input[name=password]", `Chan0228`)
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click("span.auth0-label-submit")
+        ])
+        await page.locator("a.toolbar-icon-user").waitFor()
+        await page.context().storageState({ path: 'resources/authStateDevCMS.json' })
+    })
+
+    test('expire courses', async ({ page }) => {
+        for (let index = 0; index < data.length; index++) {
+            let url = `${data[index]}`
+            await page.goto(url)
+            const expired = await page.locator("//div[@class='field__label' and text()='Expired']/following-sibling::*").innerText()
+            if (expired === "No") {
+                url = `${data[index]}/edit`
+                await page.goto(url)
+                await page.locator("summary[aria-controls=edit-group-b]").click()
+                await page.locator("label[for=edit-field-expired-value]").click()
+                await page.locator("input#edit-submit").click()
+                await page.locator("div.messages.messages--status").waitFor()
+            }
+        }
+    })
+
+    test.only('update apply url to include alchemer form', async ({ page }) => {
+        for (let index = 0; index < data.length; index++) {
+            const url = `${data[index]}/edit`
+            await page.goto(url)
+            const prefixUrl = "https://forms.prosple.com/s3/student-information?link="
+            const sourceUrl = await page.locator("input[data-drupal-selector=edit-field-record-source-url-0-uri]").inputValue()
+            const domesticApplyUrl = await page.locator("input[data-drupal-selector=edit-field-dom-apply-by-url-0-uri]").inputValue()
+            const internationalApplyUrl = await page.locator("input[data-drupal-selector=edit-field-int-apply-by-url-0-uri]").inputValue()
+            const destinationUrl = sourceUrl === "" ? sourceUrl.replace(/https?:\/\//, "") : domesticApplyUrl.replace(/https?:\/\//, "")
+            const courseName = await page.locator("input[data-drupal-selector=edit-title-0-value]").inputValue()
+            const encodedCourse = encodeURIComponent(courseName)
+            let institutionName = await page.locator("input[data-drupal-selector=edit-field-parent-institution-0-target-id]").inputValue()
+            institutionName = institutionName.replace(/ \(\d+\)$/, "").trim()
+            const encodedInstitution = encodeURIComponent(institutionName)
+            const new_url = `${prefixUrl}${destinationUrl}&institution=${encodedInstitution}&course=${encodedCourse}`
+            await page.locator("summary[aria-controls=edit-group-apply]").click()
+            await page.locator("input[data-drupal-selector=edit-field-dom-apply-by-url-0-uri]").fill(new_url)
+            if (internationalApplyUrl !== "") await page.locator("input[data-drupal-selector=edit-field-int-apply-by-url-0-uri]").fill(new_url)
+            await page.locator("input#edit-submit").click()
+            await page.locator("div.messages.messages--status").waitFor()
+        }
+    })
+})
